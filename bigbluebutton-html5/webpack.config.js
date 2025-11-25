a126e66d950e47dcab546067f3803aa59e93dffd
Gustavo Trott <gustavo@trott.com.br>
2025-11-25 13:48:40 -0300

    create chuncks

--- bigbluebutton-html5/webpack.config.js
+++ bigbluebutton-html5/webpack.config.js
@@ -32,9 +32,6 @@ const config = {
   },
   devtool: 'source-map',
   plugins: [
-    new webpack.optimize.LimitChunkCountPlugin({
-      maxChunks: 1,
-    }),
     new HtmlWebpackPlugin({
       template: './client/main.html',
       filename: 'index.html',
