f023035abf69827a6140378d433533b91fe28b01
Gustavo Trott <gustavo@trott.com.br>
2025-11-26 06:31:09 -0300

    keep chunks, take care of it in a different PR

--- bigbluebutton-html5/webpack.config.js
+++ bigbluebutton-html5/webpack.config.js
@@ -32,6 +32,9 @@ const config = {
   },
   devtool: 'source-map',
   plugins: [
+    new webpack.optimize.LimitChunkCountPlugin({
+      maxChunks: 1,
+    }),
     new HtmlWebpackPlugin({
       template: './client/main.html',
       filename: 'index.html',
